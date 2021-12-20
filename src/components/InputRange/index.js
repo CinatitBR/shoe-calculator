1ce6bd7acdd20b3368d7a338d8d157eb385a2d76
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-12-20 00:18:19 -0300

    Define Bubble position on the input range

--- src/components/InputRange/index.js
+++ src/components/InputRange/index.js
@@ -1,18 +1,39 @@
+import { useEffect, useRef } from 'react'
+
 import style from './style.module.css'
 
-const InputRange = ({ label, name, min, max, value, onChange }) => {
+const InputRange = ({ label, name, min, max, value, unit, onChange }) => {
+  const bubbleRef = useRef(null)
+
+  // Update bubble position when input value changes
+  useEffect(() => {
+    const bubbleEle = bubbleRef.current
+
+    if (bubbleEle) {
+      const newValue = Number(((value - min) * 100) / (max - min))
+
+      bubbleEle.style.left = `calc(${newValue}% + (${8 - newValue * 0.15}px))`
+    }
+  })
+
   return (
     <label className={style.wrapper}>
       {label}
+      
+      <div className={style.sliderWrapper}>
+        <input 
+          type="range" 
+          min={min} 
+          max={max}
+          name={name}
+          value={value} 
+          onChange={onChange}
+        />
 
-      <input 
-        type="range" 
-        min={min} 
-        max={max}
-        name={name}
-        value={value} 
-        onChange={onChange}
-      />
+        <div className={style.bubble} ref={bubbleRef}>
+          {value} {unit}
+        </div>
+      </div>
     </label>
   )
 }
